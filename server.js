213e7efd3709df48c08b8d26f0f78afc476e888c
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-09-24 22:33:54 +0530

    db

--- server.js
+++ server.js
@@ -2,25 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const app =express();
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:2701/myDB")
-.catch((err)=> console.log(err));
+mongoose
+    .connect("mongodb://localhost:27017/myDB")
+    .catch((err) => console.log(err));
 
 //DB SCHEMA AND MODEL
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String
+})
 
-app.get("/",(req,res)=>{
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", (req, res) => {
     res.send("Express is here");
 });
 
-app.post("/create",(req,res)=>{
-    console.log(req.body);
+app.post("/create", (req, res) => {
+   Post.create({
+    title:req.body.title,
+    description:req.body.description,
+   })
+   .then((doc)=>console.log(doc))
+   .catch((err)=>console.log(err));
 })
 
-app.listen(3001,function(){
+app.listen(3001, function () {
     console.log("Server is running");
 });
